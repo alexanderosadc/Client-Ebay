4bcc8c60afd7c01dd9e439e9d68e3c836599af4f
Alex Osadcneco <alexanderosadc@gmail.com>
2022-04-29 13:30:39 +0300

    [init] Error pages for status codes from server

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component'; 
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
@@ -17,8 +17,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
-  ],
-  exports: [ShopComponent]
+    ShopRoutingModule
+  ]
 })
 export class ShopModule { }
